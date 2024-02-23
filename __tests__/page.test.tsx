7f98f43b531446c3d4306d3bfd522f08255014f3
Earthyy <elardi2001@gmail.com>
2024-02-23 10:17:11 +0000

    fix: mock flagsmith service

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -2,9 +2,22 @@ import { render, screen } from "@testing-library/react";
 import Home from "../src/app/page";
 import "@testing-library/jest-dom";
 
+jest.mock("../src/app/utils/flagsmith", () => {
+  return {
+    __esModule: true,
+    default: {
+      getEnvironmentFlags: async () => {
+        return {
+          isFeatureEnabled: () => true,
+        };
+      },
+    },
+  };
+});
+
 describe("Home", () => {
-  it("renders a heading", () => {
-    render(<Home />);
+  it("renders a heading", async () => {
+    render(await Home());
 
     const docH = screen.getByRole("heading", {
       name: "Hello World",
